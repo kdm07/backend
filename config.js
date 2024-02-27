878916eb4dd9b015d0e0e4d3310a15ae4daf8626
pavantechie1862 <pavanmarapalli171862@gmail.com>
2024-02-27 19:05:04 +0530

    2 database instances created

--- config.js
+++ config.js
@@ -1,29 +1,21 @@
 const { createPool } = require("mysql");
 
-// const pool = createPool({
-//   user: "root",
-//   host: "localhost",
-//   password: "uday0403",
-//   connectionLimit: 10,
-//   database: "lims",
-//   // connectTimeout: 20000, // Adjust this value as needed
-// });
-
+// Production DB
 // const pool = createPool({
 //   user: "admin",
-//   host: "lims-db.cpscuwgkqwm8.ap-south-1.rds.amazonaws.com",
+//   host: "database-1.c98oisyqu0gw.ap-south-1.rds.amazonaws.com",
 //   password: "rgukt123",
 //   connectionLimit: 10,
-//   database: "lims",
-//   // connectTimeout: 20000, // Adjust this value as needed
+//   database: "productionDB",
 // });
 
+// Test DB
 const pool = createPool({
   user: "admin",
   host: "lims-db.cpscuwgkqwm8.ap-south-1.rds.amazonaws.com",
   password: "rgukt123",
   connectionLimit: 10,
-  database: "dongalims",
+  database: "lims",
 });
 
 module.exports = pool;
