4e07f8370779f0bfb45b6c48d0b1fe706e703fb8
pavantechie1862 <pavanmarapalli171862@gmail.com>
2024-02-29 16:32:02 +0530

    before hosting

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 const { createPool } = require("mysql");
 
 // Production DB
-<<<<<<< Updated upstream
 // const pool = createPool({
 //   user: "admin",
 //   host: "database-1.c98oisyqu0gw.ap-south-1.rds.amazonaws.com",
@@ -11,27 +10,21 @@ const { createPool } = require("mysql");
 // });
 
 // Test DB
-// const pool = createPool({
-//   user: "admin",
-//   host: "lims-db.cpscuwgkqwm8.ap-south-1.rds.amazonaws.com",
-//   password: "rgukt123",
-//   connectionLimit: 10,
-//   database: "lims",
-// });
-
-const pool = createPool({
-  user: "root",
-  host: "127.0.0.1",
-  password: "uday0403",
-=======
 const pool = createPool({
   user: "admin",
-  host: "database-1.c98oisyqu0gw.ap-south-1.rds.amazonaws.com",
+  host: "lims-db.cpscuwgkqwm8.ap-south-1.rds.amazonaws.com",
   password: "rgukt123",
->>>>>>> Stashed changes
   connectionLimit: 10,
-  database: "productionDB",
+  database: "lims",
 });
+
+// const pool = createPool({
+//   user: "admin",
+//   host: "database-1.c98oisyqu0gw.ap-south-1.rds.amazonaws.com",
+//   password: "rgukt123",
+//   connectionLimit: 10,
+//   database: "productionDB",
+// });
 
 // Test DB
 // const pool = createPool({
