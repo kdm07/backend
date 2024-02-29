09533f360a4983c3cccf51f34021c5ed963275a2
kdm07 <kdmengineers07@gmail.com>
2024-02-29 17:24:24 +0530

    ltst

--- config.js
+++ config.js
@@ -1,30 +1,13 @@
 const { createPool } = require("mysql");
 
 // Production DB
-// const pool = createPool({
-//   user: "admin",
-//   host: "database-1.c98oisyqu0gw.ap-south-1.rds.amazonaws.com",
-//   password: "rgukt123",
-//   connectionLimit: 10,
-//   database: "productionDB",
-// });
-
-// Test DB
 const pool = createPool({
   user: "admin",
-  host: "lims-db.cpscuwgkqwm8.ap-south-1.rds.amazonaws.com",
+  host: "database-1.c98oisyqu0gw.ap-south-1.rds.amazonaws.com",
   password: "rgukt123",
   connectionLimit: 10,
-  database: "lims",
+  database: "productionDB",
 });
-
-// const pool = createPool({
-//   user: "admin",
-//   host: "database-1.c98oisyqu0gw.ap-south-1.rds.amazonaws.com",
-//   password: "rgukt123",
-//   connectionLimit: 10,
-//   database: "productionDB",
-// });
 
 // Test DB
 // const pool = createPool({
@@ -38,9 +21,9 @@ const pool = createPool({
 // const pool = createPool({
 //   user: "root",
 //   host: "127.0.0.1",
-//   password: "keka@3061",
+//   password: "uday0403",
 //   connectionLimit: 10,
-//   database: "ramesh_sir_lims",
+//   database: "lims",
 // });
 
 module.exports = pool;
